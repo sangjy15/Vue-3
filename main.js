0455ecb42830bc2c64c98b0444759e390cd977d7
sangjy15 <40661788+sangjy15@users.noreply.github.com>
2021-11-19 16:30:58 +0900

    first commit

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            discription: 'worm and fancy shocks'
+        }
+    }
+})
